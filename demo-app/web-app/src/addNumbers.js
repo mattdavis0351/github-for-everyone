f6b44a28c0b68c7f2934bca828cd15d04486f1b6
Matthew Davis <38021615+mattdavis0351@users.noreply.github.com>
2022-02-25 11:22:38 -0800

    Update addNumbers.js

--- demo-app/web-app/src/addNumbers.js
+++ demo-app/web-app/src/addNumbers.js
@@ -1,13 +1,13 @@
 // function to parse req body and return the sum of two numbers
 function addNumbers(num1, num2) {
   let sum = {};
-  // if (typeof num1 === "number" && typeof num2 === "number") {
-  //   sum = { value: num1 + num2, msg: "success" };
-  // } else {
-  //   sum = { msg: "invalid" };
-  // }
-  sum.value = num1 + num2;
-  sum.msg = "success";
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    sum = { value: num1 + num2, msg: "success" };
+  } else {
+    sum = { msg: "invalid" };
+  }
+//   sum.value = num1 + num2;
+//   sum.msg = "success";
   return sum;
 }
 
